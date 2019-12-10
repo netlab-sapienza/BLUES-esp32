a1afdf16480ccafd09815582cd89b05a5ad6691d
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2019-12-10 12:46:06 +0100

    Updated status docs

--- components/bemesh/include/__status.h
+++ components/bemesh/include/__status.h
@@ -6,7 +6,20 @@
 #pragma once
 
 namespace bemesh{
+  // Standardized return code for bemesh functions.
+  // If a new return code has to be added, please
+  // refer to this list, and eventually update it
+  // accordingly
+
+  // Errors shall be identifies by negative values
+  // while warning or success values shall be
+  // positive.
   enum ErrStatus{
+    //TODO
+    NoCallbackError=-2,
+    BufferFullError=-1,
+    // Only warnings or succesful status below-------
     Success=0,
+    //TODO
   };
 }
