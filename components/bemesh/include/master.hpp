bb9f0dc3cf2bb3b73267a316b803cc4991b26633
DelVecchioAndrea <delvecchio.andrea97@gmail.com>
2019-12-27 17:49:07 +0100

    defined master basic structure

--- components/bemesh/include/master.hpp
+++ components/bemesh/include/master.hpp
@@ -40,12 +40,17 @@ namespace  bemesh{
 
 
     class Master{
-
-            uint8_t masterId;
-            std::string TAG;
+            std::string name;
             const int MAX_NUM_CLIENTS = 7;
-            gatts_profile_inst gl_profile_tab[NUM];
-            //NextIdService nextIdService;
+            
+            bool esp;
+            bool connected_to_internet;
+            dev_addr_t address;
+            
+
+            uint8_t master_id; //for Android compatibility mode.
+
+
 
             
             
@@ -53,10 +58,39 @@ namespace  bemesh{
             uint8_t adv_config_done = 0;
 
             public:
+                
+                
+                //to be deleted
                 Master(uint8_t id, std::string TAG);
                 Master(uint8_t id);
-                std::string getTAG();
-                uint8_t getId();
+
+
+                ~Master();
+                Master(bool is_esp, bool connected_to_internet);
+
+
+                
+                
+                std::string get_name();
+                void set_name(std::string name);
+                
+                uint8_t get_id();
+                void set_id(uint8_t new_id);
+
+
+                bool is_connected_to_internet();
+                void set_connected_to_internet(bool connected_to_internet);
+
+                bool is_esp();
+                void set_esp(bool is_esp);
+
+                dev_addr_t get_dev_addr();
+                void set_dev_addr(dev_addr_t new_dev_addr);
+
+
+
+
+
                 void init();
                 /*
                 NextIdService getNextIdService(){return nextIdService;}
--- components/bemesh/src/master/master.cpp
+++ components/bemesh/src/master/master.cpp
@@ -4,25 +4,70 @@
 
 
 namespace bemesh{
-    void gap_event_handler_trampoline(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param){}
+  
 
-    /*
-    void Master::sayHello(){
-        std::cout<<"Hello Master"<<std::endl;
-    }*/
-
-    Master::Master(uint8_t id, std::string ta):masterId(id),TAG(ta){ 
+    Master::Master(uint8_t id, std::string name):name(name),master_id(id){ 
     }
 
-    Master::Master(uint8_t id):masterId(id){
+    Master::Master(uint8_t id):master_id(id){
        
     }
 
+    Master::~Master(){
 
-    std::string Master::getTAG(){return TAG;}
+    }
 
-    uint8_t Master::getId(){return masterId;}
 
+    std::string Master::get_name(){
+        return name;
+    }
+
+    void Master::set_name(std::string new_name){
+        name = new_name;
+        return;
+    }
+
+    uint8_t Master::get_id(){
+        return master_id;
+    }
+
+    void Master::set_id(uint8_t id){
+        master_id = id;
+        return;
+    }
+
+
+     bool Master::is_connected_to_internet(){
+        return connected_to_internet;
+    }
+
+    void Master::set_connected_to_internet(bool connected){
+        connected_to_internet = connected;
+        return;
+    }
+
+
+    bool Master::is_esp(){
+        return esp;
+    }
+
+    void Master::set_esp(bool is_esp){
+        esp = is_esp;
+        return;
+    }
+
+    dev_addr_t Master::get_dev_addr(){
+        return address;
+    }
+
+    void Master::set_dev_addr(dev_addr_t dev_addr){
+        if(esp)
+            address = dev_addr;
+    }
+
+
+
+    
 
 
 
